3059c026b5563676a89fe43493730a8636cfa4ae
fferrero <fnferrero97@yahoo.com>
2024-02-22 20:40:03 -0300

    - Added swagger to launch options. - Added controller for teams and divisions. - Added services for teams and divisions. - Updated nuget packages. - Added AutoMapper.

---
+++ Club12-Backend/Apps/Club-12/AutoMapperProfiles/AutoMapperProfiles.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Club12.Entities.DivisionEntity;
+using Club12.Entities.TeamEntity;
+using Club12.Viewmodels.Division;
+using Club12.Viewmodels.Team;
+
+namespace Club12.AutoMapperProfiles;
+
+/// <summary>
+/// Mapping profiles for AutoMapper.
+/// </summary>
+public class Club12MapperProfile : Profile
+{
+    /// <summary>
+    /// All the mapping profiles and definitions.
+    /// </summary>
+    public Club12MapperProfile()
+    {
+        _ = CreateMap<Team, TeamResponse>()
+            .ReverseMap();
+
+        _ = CreateMap<TeamRequest, Team>();
+
+        _ = CreateMap<Division, DivisionResponse>()
+            .ReverseMap();
+
+        _ = CreateMap<DivisionRequest, Division>();
+    }
+}
