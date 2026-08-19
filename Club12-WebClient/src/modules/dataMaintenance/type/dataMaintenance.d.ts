427a5ddc80c72ddfd4e494bfd90b235b853c996a
FrancoRu <francoruggeri19@gmail.com>
2026-08-18 22:48:08 -0300

    feat(frontend): add dataMaintenance service

---
+++ Club12-WebClient/src/modules/dataMaintenance/type/dataMaintenance.d.ts
@@ -0,0 +1,26 @@
+export interface IDataWipeResult {
+  tournaments: number;
+  divisions: number;
+  teams: number;
+  players: number;
+  matches: number;
+  matchSeries: number;
+  playerSanctions: number;
+  playerStatistics: number;
+  scorers: number;
+  stageTeamMatches: number;
+  playerTeamRegistrations: number;
+  stages: number;
+  venues: number;
+  blogPosts: number;
+}
+
+export interface IDataSeedResult {
+  tournaments: number;
+  divisions: number;
+  teams: number;
+  players: number;
+  matches: number;
+  playerSanctions: number;
+  blogPosts: number;
+}
